3e3e22621d5e0954060a0e2401d3316e31f13ea2
Edson Silva <edsonlead@gmail.com>
2013-08-23 18:17:12 -0300

    Update and rename edsonShell.sh to installShell.sh
    alterações aplicadas(menu principal, tratamento de telas,troca de nome do aplicativo)

--- installShell.sh
+++ installShell.sh
@@ -2,17 +2,20 @@
 #
 #####[FICHA]#############################
 #					#
-# Nome: edsonShell			#
+# Nome: installShell			#
 # Versão: 2.0				#
 # Autor: Edson Araújo			#
 # Blog: edsonlead.blog.br		#
 #					#
 # ------------------------------------- #
 #					#
-# Descrição: Script desenvolvido para	#
+# DescriçãoV1: Script desenvolvido para	#
 # facilitar a instalação do LAMP, seus	#
 # componentes separados e o CMS Joomla.	#
-# 					#
+# ------------------------------------  #
+# DescriçãoV2: alterações aplicadas(	#
+# menu principal, tratamento de telas,	#
+# troca de nome do aplicativo)		#
 #########################################
 while : ; do
 menu=$(dialog --stdout \
@@ -45,6 +48,6 @@ case $menu in
 			Voltar) anterior=Instalação;;
 			Sair) dialog --title 'Você escolheu Sair' --msgbox 'Obrigado por utilizar este Aplicativo\nAcesse: edsonlead.blog.br' 6 40 ; break;;
 		esac;;
-	Sobre) dialog --title 'Sobre' --msgbox 'Nome: edsonShell\nVersão: 2.0\nAutor: Edson Araújo\nBlog: edsonlead.blog.br' 10 40 ;
+	Sobre) dialog --title 'Sobre' --msgbox 'Nome: installShell\nVersão: 2.0\nAutor: Edson Araújo\nBlog: edsonlead.blog.br' 10 40 ;
 esac
 done
